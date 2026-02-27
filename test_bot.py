ff81f13d314894048ef067563222e277b8de6c8b
Antonysrm Nafi <nafiziqbalsn@gmail.com>
2026-02-27 10:56:24 +0600

    Create test_bot.py

---
+++ test_bot.py
@@ -0,0 +1,11 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = os.environ.get("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Test bot is working!")
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+app.run_polling()
